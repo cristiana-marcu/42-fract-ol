0022b7051aacab007ae6525bf80f00c139de6782
cristiana-marcu <cristiana.marcu.n@gmail.com>
2021-11-20 20:45:18 +0100

    I'm already seein Mandelbrot

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: cristianamarcu <cristianamarcu@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 19:35:58 by RAMON             #+#    #+#             */
-/*   Updated: 2021/11/18 20:59:11 by cristianama      ###   ########.fr       */
+/*   Updated: 2021/11/20 20:09:07 by cristianama      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 # include "mlx.h"
 # include <stdlib.h>
+# include <math.h>
+# include <stdio.h>
 
 //Keys
 # define K_ESC	53
@@ -22,5 +24,44 @@
 //Window
 # define WIN_WIDTH 1280
 # define WIN_HEIGHT 720
+
+enum	e_fractal{MANDELBROT, JULIA};
+
+//Structures
+typedef struct	s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
+
+typedef struct	s_viewport
+{
+	double	xmin;
+	double	xmax;
+	double	ymin;
+	double	ymax;
+}	t_viewport;
+
+typedef struct s_fractal
+{
+	enum e_fractal	name;
+	t_viewport		viewport;
+	int				max_iter;
+}	t_fractal;
+
+typedef struct	s_mlx {
+	void		*mlx;
+	void		*win;
+	t_fractal	*fractal;
+}	t_mlx;
+
+typedef struct	s_complex
+{
+	double	r;
+	double	i;
+}	t_complex;
 
 #endif
--- main.c
+++ main.c
@@ -6,26 +6,12 @@
 /*   By: cristianamarcu <cristianamarcu@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 18:29:10 by cristianama       #+#    #+#             */
-/*   Updated: 2021/11/18 21:48:06 by cristianama      ###   ########.fr       */
+/*   Updated: 2021/11/20 20:28:24 by cristianama      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "mlx.h"
+//#include "mlx.h"
 #include "fractol.h"
-
-typedef struct	s_data
-{
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}	t_data;
-
-typedef struct	s_mlx {
-	void	*mlx;
-	void	*win;
-}				t_mlx;
 
 int	create_trgb(int t, int r, int g, int b)
 {
@@ -47,10 +33,49 @@ int hook_keydown(int key) //Futuro parámetro "t_mlx *mlx" para hacer zoom con t
 	return (0);
 }
 
-void render_next_frame(t_data *img) //Futuro parámetro "t_mlx *mlx" para hacer aquí el put_image_to_window
+int mandelbrot(t_mlx *mlx, t_complex z, t_complex c)
 {
-	int x;
-	int y;
+	int		i;
+	double	n;
+	double	temp;
+
+	i = 0; //Possible to initialize it at -1 if I get short on lines and in the loop ++i
+	n = z.r * z.r + z.i * z.i;
+	temp = 0;
+	while (i < mlx->fractal->max_iter && n < 4)
+	{
+		temp = z.r;
+		z.r = z.r * z.r - z.i * z.i + c.r;
+		z.i = 2 * z.i * temp + c.i;
+		n = z.r * z.r + z.i * z.i;
+		i++;
+	}
+	return (i);
+}
+
+void fill_pixel(int x, int y, t_data *img, t_mlx *mlx)
+{
+	t_complex	z;
+	t_complex	c;
+	int			i;
+
+	//TODO find a better way to convert pixel to complex
+	c.r = (double)x / 400 + mlx->fractal->viewport.xmin;
+	c.i = (double)y / 400 + mlx->fractal->viewport.ymin;
+	z = (t_complex) {.r = 0, .i = 0};
+	i = 0;
+	if (mlx->fractal->name == MANDELBROT)
+		i = mandelbrot(mlx, z, c);
+	if (i == mlx->fractal->max_iter)
+		my_mlx_pixel_put(img, x, y, create_trgb(0, 187, 0, 47));
+	else
+		my_mlx_pixel_put(img, x, y, create_trgb(0, 0, 187, 47));
+}
+
+void fractol(t_data *img, t_mlx *mlx)
+{
+	int	x;
+	int	y;
 
 	y = 0;
 	while (y < WIN_HEIGHT)
@@ -58,39 +83,94 @@ void render_next_frame(t_data *img) //Futuro parámetro "t_mlx *mlx" para hacer 
 		x = 0;
 		while (x < WIN_WIDTH)
 		{
-			my_mlx_pixel_put(img, x, y, create_trgb(0, 187, 0, 47));
+			fill_pixel(x, y, img, mlx);
 			x++;
 		}
 		y++;
 	}
+	mlx_put_image_to_window(mlx->mlx, mlx->win, img->img, 0, 0);
 }
 
-t_mlx	*init()
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	a;
+
+	a = 0;
+	while (n && s1[a] != '\0' && s1[a] == s2[a])
+	{
+		a++;
+		--n;
+	}
+	if (n == 0)
+		return (0);
+	else
+		return ((int)(unsigned char)(s1[a]) - (int)(unsigned char)(s2[a]));
+}
+
+
+int	match_fractal(t_mlx *mlx, char *argv)
+{
+	mlx->fractal = (t_fractal*)malloc(sizeof(t_fractal));
+	if (!ft_strncmp(argv, "Mandelbrot", 10))
+	{
+		mlx->fractal->name = MANDELBROT; //TODO: segfault here without line 74
+		mlx->fractal->viewport.xmax = 1.0;
+		mlx->fractal->viewport.ymax = 1.0;
+		mlx->fractal->viewport.xmin = -2.0;
+		mlx->fractal->viewport.ymin = -1.0;
+		mlx->fractal->max_iter = 80;
+	}
+	else
+		return (0);
+	return (1);
+}
+
+void	print_error(char *message)
+{
+	printf("Error - %s \n", message);
+	exit(1);
+}
+
+t_mlx	*init(char *argv)
 {
 	t_mlx	*mlx;
-	
+	//char	*title;
+
+	//title = ft_strjoin("Fract'ol - ", argv);
 	mlx = (t_mlx*)malloc(sizeof(t_mlx));
 	if (mlx == NULL)
 		return (NULL);
 	mlx->mlx = mlx_init();
-	mlx->win = mlx_new_window(mlx->mlx, WIN_WIDTH, WIN_HEIGHT, "Hello world!");
+	if (!mlx->mlx)
+		print_error("Failed initializing mlx");
+	mlx->win = mlx_new_window(mlx->mlx, WIN_WIDTH, WIN_HEIGHT, "Fract'ol - ");
+	if (!mlx->win)
+		print_error("Failed creating window");
+	if (!match_fractal(mlx, argv))
+		print_error("Wrong argument. Please type 'Mandelbrot'");
 	return (mlx);
 }
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	t_mlx	*mlx;
 	t_data	img;
 
-	mlx = init();
+	if (argc != 2)
+		print_error("Wrong number of arguments");
+	mlx = init(argv[1]);
+	//TODO: init_img para las dos líneas siguientes.
 	img.img = mlx_new_image(mlx->mlx, WIN_WIDTH, WIN_HEIGHT);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 				&img.endian);
-				
-	render_next_frame(&img);
+	
+	
+	fractol(&img, mlx);
 	//TODO next: una animación que con mlx_loop_hook pase por todos los colores.
 	//Bitwise operands, aprende de una vez
-	mlx_put_image_to_window(mlx->mlx, mlx->win, img.img, 0, 0);
+	
 	mlx_key_hook(mlx->win, hook_keydown, mlx->mlx);
 	mlx_loop(mlx->mlx);
 }
+
+//Places where I malloc: init function, match_fractal function
